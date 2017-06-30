15238b324c8bb8494b3aaaedf9878d4218ccdc65
Tomas Giedraitis <tomasgiedraitis@gmail.com>
2017-06-30 12:38:07 +0300

    tutorial creating component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,17 @@
+import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
-import { AppComponent } from './app.component';
-
+import { FormsModule }   from '@angular/forms'; // <-- NgModel lives here
+ 
+import { AppComponent }  from './app.component';
+ 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	imports: [
+		BrowserModule,
+		FormsModule // <-- import the FormsModule before binding with [(ngModel)]
+	],
+	declarations: [
+		AppComponent
+	],
+	bootstrap: [ AppComponent ]
 })
 export class AppModule { }
